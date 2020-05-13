471d35a8ddecd554f53fada76a13324378facc70
Mark Tinderholt <mark@vitalsigyn.com>
2020-05-12 22:09:34 -0400

    basic fn backend

---
+++ fn/variables.tf
@@ -0,0 +1,23 @@
+/* 
+ *
+ * This module creates a backend for an Azure Function
+ * 
+ *
+ *  - It assumes your Azure Function is configured with the default URL template:
+ *      
+ *        https://YOUR_FUNCTION_NAME_HERE.azurewebsites.net/api
+ * 
+ *  - It assumes that your Azure Function is configured using Azure Function 
+ *      Authentication. Therefore, it will add the 'x-functions-key' as an 
+ *      HTTP Header credential.
+ *
+ *
+ */
+variable "resource_group_name" { }
+variable "apim_name" { }
+variable "name" { }
+variable "function_name" { }
+variable "function_key" { }
+variable "protocol" { 
+  default = "http"
+}
